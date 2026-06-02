d2045a667a7bf5a6c1a6cef78085237ce2ef62ce
aleksandrmodestov <aleksandrmodestov@gmail.com>
2026-06-02 22:20:26 +0100

    feat: define FSM states for all conversation flows

---
+++ bot/states/states.py
@@ -0,0 +1,20 @@
+from aiogram.fsm.state import State, StatesGroup
+
+class ToneOfVoiceStates(StatesGroup):
+    waiting_role = State()
+    waiting_audience = State()
+    waiting_style = State()
+    waiting_examples = State()
+    confirming_profile = State()
+
+class LinkedInStates(StatesGroup):
+    collecting_inputs = State()
+    editing = State()
+
+class TikTokStates(StatesGroup):
+    waiting_subtype = State()
+    waiting_product_url = State()
+    recording = State()
+    waiting_description = State()
+    collecting_materials = State()
+    generating_video = State()
